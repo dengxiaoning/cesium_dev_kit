f9800cf2456d343dd4f1cb760aa888abfa842e61
dengxiaoning <412693453@qq.com>
2023-03-02 17:22:03 +0800

    增加动态墙、发光球体材质

---
+++ src/views/example/material/dynamicWall.vue
@@ -0,0 +1,139 @@
+<template>
+  <div>
+    <div id="cesiumContainer" class="map3d-contaner"></div>
+  </div>
+</template>
+<script>
+  import * as Cesium from 'cesium'
+  import { initCesium } from '@/utils/cesiumPluginsExtends/index'
+
+  export default {
+    mounted() {
+      this.initMap()
+    },
+    methods: {
+      initMap() {
+        const { viewer, material, graphics, math3d } = new initCesium(
+          Cesium,
+          'cesiumContainer',
+          {
+            infoBox: false,
+            shouldAnimate: true
+          },
+          []
+        )
+
+        this.c_viewer = viewer
+
+        this.material = material
+        this.graphics = graphics
+        this.math3d = math3d
+        // let layer = this.c_viewer.imageryLayers.addImageryProvider(new Cesium.Scene.BaiduImageryProvider({
+        //   style: 'dark'
+        // }));
+        this.material.setDefSceneConfig()
+        this.material.setBloomLightScene()
+        let tileset = this.c_viewer.scene.primitives.add(
+          new Cesium.Cesium3DTileset({
+            url: 'static/data/3DTiles/building/tileset.json'
+          })
+        )
+        tileset.style = new Cesium.Cesium3DTileStyle({
+          color: {
+            conditions: [
+              ['${height} >= 300', 'rgba(0, 149, 251, 0.3)'],
+              ['${height} >= 200', 'rgb(0, 149, 251, 0.3)'],
+              ['${height} >= 100', 'rgb(0, 149, 251, 0.3)'],
+              ['${height} >= 50', 'rgb(0, 149, 251, 0.3)'],
+              ['${height} >= 25', 'rgb(0, 149, 251, 0.3)'],
+              ['${height} >= 10', 'rgb(0, 149, 251, 0.3)'],
+              ['${height} >= 5', 'rgb(0, 149, 251, 0.3)'],
+              ['true', 'rgb(0, 149, 251, 0.3)']
+            ]
+          }
+        })
+
+        this.c_viewer.flyTo(tileset)
+
+        this.createPolygonOne()
+        this.createPolygonTwo()
+      },
+      createCustMaterialWall(imgUrl, colorVal, durationNum) {
+        return this.material.getCustomMaterialWall({
+          image: imgUrl,
+          freely: 'vertical',
+          direction: '+',
+          count: 2.0,
+          color: colorVal,
+          duration: durationNum
+        })
+      },
+      createPolygonOne() {
+        var polygon = this.graphics.createPolygonGraphics({
+          positions: Cesium.Cartesian3.fromDegreesArray([
+            104.09816110606057,
+            30.659821965447698,
+            104.1120972824757,
+            30.65330300319938,
+            104.10758419863926,
+            30.64592470850288,
+            104.09351691196979,
+            30.652434826507452,
+            104.09816110606057,
+            30.659821965447698
+          ]),
+          material: this.createCustMaterialWall(
+            'static/data/images/Textures/color3.png',
+            Cesium.Color.BLUE,
+            50000
+          )
+        })
+        polygon.polygon.extrudedHeight = 500
+      },
+      createPolygonTwo() {
+        var polygon2 = this.graphics.createPolygonGraphics({
+          positions: Cesium.Cartesian3.fromDegreesArray([
+            104.07263175401185,
+            30.647622150198725,
+            104.06369117158526,
+            30.648834374000277,
+            104.06437182811021,
+            30.62274533905387,
+            104.07463538167119,
+            30.62285687644371,
+            104.07263175401185,
+            30.647622150198725
+          ]),
+          material: this.createCustMaterialWall(
+            'static/data/images/Textures/gradual_change_red_02.png',
+            Cesium.Color.BLUE.withAlpha(0.5),
+            20000
+          )
+        })
+        polygon2.polygon.extrudedHeight = 500
+      }
+    },
+    beforeUnmount() {
+      this.c_viewer = null
+      this.material = null
+      this.graphics = null
+    }
+  }
+</script>
+<style lang="scss" scoped>
+  .map3d-contaner {
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+    .ctrl-group {
+      position: absolute;
+      top: 10px;
+      right: 20px;
+      z-index: 999;
+      .reset-home-btn {
+        color: #36a3f7;
+        cursor: pointer;
+      }
+    }
+  }
+</style>
---
+++ src/views/example/material/sphereMaterial.vue
@@ -0,0 +1,170 @@
+<template>
+  <div>
+    <div id="cesiumContainer" class="map3d-contaner"></div>
+  </div>
+</template>
+<script>
+  import * as Cesium from 'cesium'
+  import { initCesium } from '@/utils/cesiumPluginsExtends/index'
+
+  export default {
+    mounted() {
+      this.initMap()
+    },
+    methods: {
+      initMap() {
+        const { viewer, material, graphics, math3d } = new initCesium(
+          Cesium,
+          'cesiumContainer',
+          {
+            infoBox: false,
+            shouldAnimate: true
+          },
+          []
+        )
+
+        this.c_viewer = viewer
+
+        this.material = material
+        this.graphics = graphics
+        this.math3d = math3d
+        // let layer = this.c_viewer.imageryLayers.addImageryProvider(new Cesium.Scene.BaiduImageryProvider({
+        //   style: 'dark'
+        // }));
+        this.material.setDefSceneConfig()
+        this.material.setBloomLightScene()
+        let tileset = this.c_viewer.scene.primitives.add(
+          new Cesium.Cesium3DTileset({
+            url: 'static/data/3DTiles/building/tileset.json'
+          })
+        )
+        tileset.style = new Cesium.Cesium3DTileStyle({
+          color: {
+            conditions: [
+              ['${height} >= 300', 'rgba(0, 149, 251, 0.3)'],
+              ['${height} >= 200', 'rgb(0, 149, 251, 0.3)'],
+              ['${height} >= 100', 'rgb(0, 149, 251, 0.3)'],
+              ['${height} >= 50', 'rgb(0, 149, 251, 0.3)'],
+              ['${height} >= 25', 'rgb(0, 149, 251, 0.3)'],
+              ['${height} >= 10', 'rgb(0, 149, 251, 0.3)'],
+              ['${height} >= 5', 'rgb(0, 149, 251, 0.3)'],
+              ['true', 'rgb(0, 149, 251, 0.3)']
+            ]
+          }
+        })
+
+        this.c_viewer.flyTo(tileset)
+
+        this.createEllipsoidOne()
+        this.createEllipsoidTwo()
+        this.createEllipsoidThree()
+      },
+      createCustMaterialWall(imgUrl, colorVal, durationNum, countNum) {
+        return this.material.getCustomMaterialWall({
+          image: imgUrl,
+          freely: 'cross',
+          direction: '+',
+          count: countNum,
+          color: colorVal,
+          duration: durationNum
+        })
+      },
+      // ellipsoid one
+      createEllipsoidOne() {
+        var ellipsoid = new Cesium.EllipsoidGraphics({
+          radii: new Cesium.Cartesian3(500, 500, 500), //单位 米
+          material: this.createCustMaterialWall(
+            'static/data/images/Textures/test1.png',
+            Cesium.Color.BLUE,
+            2000,
+            0.0
+          ),
+          maximumCone: Cesium.Math.PI_OVER_TWO
+        })
+        var position = Cesium.Cartesian3.fromDegrees(
+          104.081701757991,
+          30.627042558105988
+        )
+        let three1 = this.c_viewer.entities.add({
+          name: 'aaaaa',
+          position: position,
+          ellipsoid: ellipsoid
+        })
+      },
+      // ellipsoid two
+      createEllipsoidTwo() {
+        var position = Cesium.Cartesian3.fromDegrees(
+          104.06417395476578,
+          30.636185094244944
+        )
+        var ellipsoid = this.graphics.getEllipsoidGraphics({
+          radii: 700,
+          material: this.createCustMaterialWall(
+            'static/data/images/Textures/color3.png',
+            Cesium.Color.BLUE.withAlpha(0.5),
+            2000,
+            1.0
+          ),
+          outline: false,
+          outlineColor: Cesium.Color.WHITE.withAlpha(0.8)
+        })
+        ellipsoid.innerRadii = new Cesium.Cartesian3(
+          700 / 1.5,
+          700 / 1.5,
+          700 / 1.5
+        )
+
+        let three2 = this.c_viewer.entities.add({
+          name: 'aaaaa',
+          position: position,
+          ellipsoid: ellipsoid
+        })
+      },
+      // ellipsoid three
+      createEllipsoidThree() {
+        var ellipsoid = new Cesium.EllipsoidGraphics({
+          radii: new Cesium.Cartesian3(500, 500, 500), //单位 米
+          material: this.createCustMaterialWall(
+            'static/data/images/Textures/b2.png',
+            Cesium.Color.BLUE,
+            2000,
+            1.0
+          ),
+          maximumCone: Cesium.Math.PI_OVER_TWO
+        })
+        var position = Cesium.Cartesian3.fromDegrees(
+          104.05248507490258,
+          30.653854957158103
+        )
+
+        let three3 = this.c_viewer.entities.add({
+          name: 'aaaaa',
+          position: position,
+          ellipsoid: ellipsoid
+        })
+      }
+    },
+    beforeUnmount() {
+      this.c_viewer = null
+      this.material = null
+      this.graphics = null
+    }
+  }
+</script>
+<style lang="scss" scoped>
+  .map3d-contaner {
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+    .ctrl-group {
+      position: absolute;
+      top: 10px;
+      right: 20px;
+      z-index: 999;
+      .reset-home-btn {
+        color: #36a3f7;
+        cursor: pointer;
+      }
+    }
+  }
+</style>
