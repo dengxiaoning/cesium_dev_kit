ecbcbd3f25258f41526a0809d08d97f8047710ee
dengxiaoning <d1236688@163.com>
2024-10-27 17:13:47 +0800

    mod: :arrow_up: compatible with Cesium version 1.122.0

---
+++ src/views/example/graphics/rotatingDisk.vue
@@ -0,0 +1,102 @@
+<template>
+  <div id="cesiumContainer"
+       class="map3d-contaner"></div>
+</template>
+<script >
+import * as Cesium from 'cesium'
+import { initCesium } from '@/utils/cesiumPluginsExtends/index'
+
+
+export default {
+  mounted () {
+    this.initMap()
+  },
+  methods: {
+    async initMap () {
+      const {
+        viewer,
+        material,
+        graphics
+      } = new initCesium(
+        {
+          cesiumGlobal: Cesium,
+          containerId: 'cesiumContainer',
+          viewerConfig: {
+            infoBox: false,
+            shouldAnimate: true,
+          },
+          extraConfig: {},
+          MapImageryList: []
+        })
+
+
+      this.c_viewer = viewer;
+
+      this.material = material;
+      this.graphics = graphics;
+
+      this.graphics.setDefSceneConfig()
+      this.graphics.setBloomLightScene()
+      let tiles = await Cesium.Cesium3DTileset.fromUrl('static/data/3DTiles/building/tileset.json');
+      let tileset = this.c_viewer.scene.primitives.add(tiles)
+      tileset.style = new Cesium.Cesium3DTileStyle({
+        color: {
+          conditions: [
+            ["${height} >= 300", "rgba(0, 149, 251, 0.3)"],
+            ["${height} >= 200", "rgb(0, 149, 251, 0.3)"],
+            ["${height} >= 100", "rgb(0, 149, 251, 0.3)"],
+            ["${height} >= 50", "rgb(0, 149, 251, 0.3)"],
+            ["${height} >= 25", "rgb(0, 149, 251, 0.3)"],
+            ["${height} >= 10", "rgb(0, 149, 251, 0.3)"],
+            ["${height} >= 5", "rgb(0, 149, 251, 0.3)"],
+            ["true", "rgb(0, 149, 251, 0.3)"]
+          ]
+        }
+      })
+      this.createModel()
+      this.flyto()
+    },
+    flyto () {
+      this.material.flyTo({
+        position: { x: -1337132.0092982147, y: 5330611.474631115, z: 3228680.029449292 },
+        orientation: {
+          heading: Cesium.Math.toRadians(1.0114629015290062),
+          pitch: Cesium.Math.toRadians(-23.53661660731824),
+          roll: Cesium.Math.toRadians(0.00324596311071617)
+        }
+      })
+    },
+    createModel () {
+      this.graphics.craeteRotateCylinderGraphics({
+        position: Cesium.Cartesian3.fromDegrees(104.081701757991, 30.627042558105988, 1),
+        img: 'static/data/images/file/cc2m.png',
+        length: 500 / 2,
+        topRadius: 500,
+        bottomRadius: 450,
+      })
+    },
+  },
+  beforeUnmount () {
+    this.c_viewer = null;
+    this.material = null;
+    this.graphics = null;
+  }
+}
+</script>
+<style lang="scss" scoped>
+.map3d-contaner {
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+  .ctrl-group {
+    position: absolute;
+    top: 10px;
+    right: 20px;
+    z-index: 999;
+    .reset-home-btn {
+      color: #36a3f7;
+      cursor: pointer;
+    }
+  }
+}
+</style>
