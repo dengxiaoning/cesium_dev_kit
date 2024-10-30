45128e4d96f880fd3709e04403502478cac6595d
dengxiaoning <d1236688@163.com>
2024-10-30 11:13:01 +0800

    fix: :bug: resolve the exception of reading updateTransform as undefined

---
+++ src/views/example/graphics/dynamicGradientWall.vue
@@ -0,0 +1,97 @@
+<template>
+  <div id="cesiumContainer"
+       class="map3d-contaner"></div>
+</template>
+<script >
+import * as Cesium from 'cesium'
+import { Graphics } from '@/utils/cesiumPluginsExtends/singleImport/Graphics'
+
+
+export default {
+  mounted () {
+    this.initMap()
+  },
+  methods: {
+    async initMap () {
+      const graphicsObj = new Graphics({
+        cesiumGlobal: Cesium,
+        containerId: 'cesiumContainer',
+        viewerConfig: {
+          infoBox: false,
+          shouldAnimate: true,
+        },
+        MapImageryList: []
+      })
+      this.c_viewer = graphicsObj.viewer;
+
+      this.graphics = graphicsObj.graphics;
+
+      this.graphics.setDefSceneConfig()
+      this.graphics.setBloomLightScene()
+      let tiles = await Cesium.Cesium3DTileset.fromUrl('static/data/3DTiles/building/tileset.json');
+      let tileset = this.c_viewer.scene.primitives.add(tiles)
+      tileset.style = new Cesium.Cesium3DTileStyle({
+        color: {
+          conditions: [
+            ["${height} >= 300", "rgba(0, 149, 251, 0.3)"],
+            ["${height} >= 200", "rgb(0, 149, 251, 0.3)"],
+            ["${height} >= 100", "rgb(0, 149, 251, 0.3)"],
+            ["${height} >= 50", "rgb(0, 149, 251, 0.3)"],
+            ["${height} >= 25", "rgb(0, 149, 251, 0.3)"],
+            ["${height} >= 10", "rgb(0, 149, 251, 0.3)"],
+            ["${height} >= 5", "rgb(0, 149, 251, 0.3)"],
+            ["true", "rgb(0, 149, 251, 0.3)"]
+          ]
+        }
+      })
+
+      this.flyto();
+
+      this.createModel();
+    },
+    flyto () {
+      this.graphics.flyTo({
+        position: { x: -1337132.0092982147, y: 5330611.474631115, z: 3228680.029449292 },
+        orientation: {
+          heading: Cesium.Math.toRadians(1.0114629015290062),
+          pitch: Cesium.Math.toRadians(-23.53661660731824),
+          roll: Cesium.Math.toRadians(0.00324596311071617)
+        }
+      })
+    },
+    createModel () {
+      this.graphics.craeteDynamicShadeWallGraphics({
+        positions: Cesium.Cartesian3.fromDegreesArrayHeights([
+          104.07263175401185, 30.647622150198725, 500.0,
+          104.06369117158526, 30.648834374000277, 500.0,
+          104.06437182811021, 30.62274533905387, 500.0,
+          104.07463538167119, 30.62285687644371, 500.0,
+          104.07263175401185, 30.647622150198725, 500.0
+        ]),
+      })
+
+    }
+  },
+  beforeUnmount () {
+    this.c_viewer = null;
+    this.graphics = null;
+  }
+}
+</script>
+<style lang="scss" scoped>
+.map3d-contaner {
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+  .ctrl-group {
+    position: absolute;
+    top: 10px;
+    right: 20px;
+    z-index: 999;
+    .reset-home-btn {
+      color: #36a3f7;
+      cursor: pointer;
+    }
+  }
+}
+</style>
